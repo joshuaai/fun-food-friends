cad8e76092bd2a1f3f8886673a8cbda82c3f8492
generationenterprise <technology@generationenterprise.org>
2017-09-08 13:40:21 +0100

    Authentication with Firebase

--- help.md
+++ help.md
@@ -90,3 +90,80 @@ removeItem(itemId) {
 ```
 
 The Firebase documentation is a great place to find out more on [Web Database Structure and Operations](https://firebase.google.com/docs/database/web/start).
+
+## Firebase User Authentication
+* Change the Firebase `Database Rules` to require authentication as previously:
+```json
+{
+  "rules": {
+    ".read": "auth != null",
+    ".write": "auth != null"
+  }
+}
+```
+
+* Enable one of the `Authentication` options - Google, in this case, on the Firebase console.
+
+* To the `firebase.js` file, add the Google authentication provider. Add the following before the `export default firebase`:
+```js
+export const provider = new firebase.auth.GoogleAuthProvider();
+export const auth = firebase.auth();
+```
+
+* Change the main `App.js` import for firebase to
+```js
+import firebase, { auth, provider } from './firebase.js';
+```
+
+* You can then handle the `Login` state with:
+```js
+login() {
+  auth.signInWithPopup(provider) 
+    .then((result) => {
+      const user = result.user;
+      this.setState({
+        user
+      });
+    });
+}
+```
+
+* The `Logout` state can be handled with
+```js
+logout() {
+  auth.signOut()
+    .then(() => {
+      this.setState({
+        user: null
+      });
+    });
+}
+```
+
+* You can represent Login and Logout in the React state like so:
+```jsx
+<header>
+    <div className='wrapper'>
+      <h1>Fun Food Friends</h1>
+      { user ? 
+        <button onClick={this.logout}>Log Out </button>
+        :
+        <button onClick={this.login}>Log In</button>
+      }
+    </div>
+</header>
+```
+
+## Persisting Login Across Refresh
+Firebase has an event listener, `onAuthStateChange`, that can automatically sign users back in if they were previously signed in. We can do this in `componentDidMount`:
+```js
+componentDidMount() {
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      this.setState({ user });
+    } 
+  });
+  // ...
+```   
+
+
--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import firebase from './firebase.js';
+import firebase, { auth, provider } from './firebase.js';
 
 class App extends Component {
   constructor() {
@@ -8,14 +8,22 @@ class App extends Component {
     this.state = {
       currentItem: '',
       username: '',
-      items: []
+      items: [],
+      user: null
     }
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.removeItem = this.removeItem.bind(this);
+    this.login = this.login.bind(this);
+    this.logout = this.logout.bind(this);
   }
 
   componentDidMount() {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ user });
+      } 
+    });
     const itemsRef = firebase.database().ref('items');
     itemsRef.on('value', (snapshot) => {
       let items = snapshot.val();
@@ -37,12 +45,31 @@ class App extends Component {
     });
   }
 
+  login() {
+    auth.signInWithPopup(provider) 
+      .then((result) => {
+        const user = result.user;
+        this.setState({
+          user
+        });
+      });
+  }
+
+  logout() {
+    auth.signOut()
+      .then(() => {
+        this.setState({
+          user: null
+        });
+      });
+  }  
+
   handleSubmit(e) {
     e.preventDefault();
     const itemsRef = firebase.database().ref('items');
     const item = {
       title: this.state.currentItem,
-      user: this.state.username
+      user: this.state.user.displayName || this.state.user.email
     }
     itemsRef.push(item);
     this.setState({
@@ -56,38 +83,57 @@ class App extends Component {
   }
 
   render() {
-    const { username, currentItem, items } = this.state;
+    const { username, currentItem, items, user } = this.state;
     return (
       <div className='app'>
         <header>
             <div className='wrapper'>
               <h1>Fun Food Friends</h1>
+              { user ? 
+                <button onClick={this.logout}>Log Out </button>
+                :
+                <button onClick={this.login}>Log In</button>
+              }
             </div>
         </header>
-        <div className='container'>
-          <section className='add-item'>
-              <form onSubmit={this.handleSubmit}>
-                <input type="text" name="username" placeholder="What's your name?" onChange={this.handleChange} value={username} />
-                <input type="text" name="currentItem" placeholder="What are you bringing?" onChange={this.handleChange} value={currentItem} />
-                <button>Add Item</button>
-              </form>
-          </section>
-          <section className='display-item'>
-            <div className="wrapper">
-              <ul>
-                {items.map((item) => {
-                  return (
-                    <li key={item.id}>
-                      <h3>{item.title}</h3>
-                      <p>brought by: {item.user}</p>
-                      <button onClick={() => this.removeItem(item.id)}>Remove Item</button>
-                    </li>
-                  )
-                })}
-              </ul>
+        { user ?
+          <div>
+            <div className='user-profile'>
+              <img src={user.photoURL} />
             </div>
-          </section>
-        </div>
+            <div className='container'>
+              <section className='add-item'>
+                  <form onSubmit={this.handleSubmit}>
+                    <input type="text" name="username" placeholder="What's your name?" value={ user.displayName || user.email } />
+                    <input type="text" name="currentItem" placeholder="What are you bringing?" onChange={this.handleChange} value={currentItem} />
+                    <button>Add Item</button>
+                  </form>
+              </section>
+              <section className='display-item'>
+                <div className="wrapper">
+                  <ul>
+                    {items.map((item) => {
+                      return (
+                        <li key={item.id}>
+                          <h3>{item.title}</h3>
+                          <p>brought by: {item.user}
+                            { (item.user === user.displayName || item.user === user.email) &&
+                              <button onClick={() => this.removeItem(item.id)}>Remove Item</button>
+                            }
+                          </p>
+                        </li>
+                      )
+                    })}
+                  </ul>
+                </div>
+              </section>
+            </div>
+          </div>
+          :
+          <div className='wrapper'>
+            <p>You must be logged in to see the potluck list and submit to it.</p>
+          </div>
+        }
       </div>
     );
   }
